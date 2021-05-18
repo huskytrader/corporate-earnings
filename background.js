8c372387c2514080b6bf99c81784aac7faa9f29c
HuskyTrader <huskytrades@gmail.com>
2021-05-18 15:35:12 +0700

    more fundamental work

--- background.js
+++ background.js
@@ -1,46 +1,61 @@
+const SA_REGEX = /symbol\/([a-zA-Z]+)\/earnings/;
+const ZA_REGEX = /stock\/research\/([a-zA-Z]+)\/earnings-announcements/;
+const URL_PREFIX = 'https://finviz.com/';
+const CHROME_PREFIX_REGEX = /chrome-extension:\/\/\w+\//;
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log("message received: ", message);
-   
-
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var regex = /symbol\/([a-zA-Z]+)\/earnings/;
+        let symbol = "";
         let url = tabs[0].url;
-        let res = regex.exec(url);
-        let symbol = res[1];
-        getFundamentals(symbol, sendResponse);
-    }); 
-        
-    return true;    
-        //sendResponse(fundamentals);
-    
+        let res = SA_REGEX.exec(url);
+        if (res != null) {
+            symbol = res[1];
+            getFundamentals(symbol, sendResponse);
+            return true;
+        } 
+        res = ZA_REGEX.exec(url);
+        if (res != null) {
+            symbol = res[1];
+            getFundamentals(symbol, sendResponse);
+            return true;
+        }
+        sendResponse({error: 'failed to fetch fundamentals'});
+
+    });     
+    return true;        
 });
-
-function getSymbol() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var regex = /symbol\/([a-zA-Z]+)\/earnings/;
-        let url = tabs[0].url;
-        let res = regex.exec(url);
-        return res[1];
-    }); 
-}
 
 function getFundamentals(symbol, sendResponse) {
     if (! isDefined(symbol)) { return undefined; }
     let results = {symbol: symbol};
-
-    console.log('queriyng Finviz for symbol: ', symbol);
 
     fetch('https://finviz.com/quote.ashx?t='+symbol)
     .then(function(response) {
         return response.text()
     })
     .then(function(html) {
-        var dom_nodes = $($.parseHTML(html));
+        let dom_nodes = $($.parseHTML(html));
 
-        let found = dom_nodes.find('.fullview-title');
+        found = dom_nodes.find('.fullview-title tr:eq(0) td');
         if (found) {
-            results.title = found.html();
+            results.ticker = found.children(':first-child').text();
+            results.tickerHref = stripChromePrefix(found.children(':first-child').prop('href'));
+            results.exchange = found.children(':first-child').next().text();
         }
+        found = dom_nodes.find('.fullview-title tr:eq(1) td');
+        if (found) {
+            results.site = found.html();
+        }
+        found = dom_nodes.find('.fullview-title tr:eq(2) td');
+        if (found) {
+            results.sector = found.children(':first-child').text();
+            results.sectorHref = stripChromePrefix(found.children(':first-child').prop('href'));
+            results.industry = found.children(':first-child').next().text();
+            results.industryHref = stripChromePrefix(found.children(':first-child').next().prop('href'));
+            results.country = found.children(':first-child').next().next().text();
+            results.countryHref = stripChromePrefix(found.children(':first-child').next().next().prop('href'));
+        }
+
         found = dom_nodes.find('td:contains("Short Float")');
         if (found) {
             results.shorts = found.next().text().trim();
@@ -77,23 +92,63 @@ function getFundamentals(symbol, sendResponse) {
         if (found) {
             results.description = found.text().trim();
         }
+        found = dom_nodes.find('.fullview-ratings-outer');
+        if (found) {
+            results.ratings = found.prop('outerHTML');
+            results.ratings = results.ratings.replace(/<b>/g, "");
+            results.ratings = results.ratings.replace(/<\/b>/g, "");
+        }
+        found = dom_nodes.find('.fullview-news-outer');
+        if (found) {
+            results.news = found.prop('outerHTML');
+        }
+        found = dom_nodes.find('td:contains("Insider Trading")');
+        if (found) {
+            results.insiders = processInsiders(found.closest('.body-table').prop('outerHTML'));
+        }
 
-        console.log(results);
-        //chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        //    chrome.tabs.sendMessage(tabs[0].id, results, function(response){});
-        //});
-        sendResponse(results);
+        fetch('https://charts2.finviz.com/chart.ashx?t='+symbol+'&ty=c&ta=1&p=d&s=l')
+            .then(response => response.arrayBuffer())
+            .then(buf => {
+                    results.chart = arrayBufferToBase64(buf);
+                    sendResponse(results);
+                })
+            .catch (err => {
+                console.log('Failed to fetch image: ', err);
+                sendResponse(results);
+            });
+
     })
     .catch(function(err) {  
         console.log('Failed to fetch page: ', err);  
         results.error = err;
-        //chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        //    chrome.tabs.sendMessage(tabs[0].id, results, function(response){});
-        //});
-        sendResponse({error: 'failed to fetch fundamentals for symbol: ${symbol}'});
+        sendResponse({error: 'failed to fetch fundamentals for symbol: ' + symbol});
     });
+}
+
+// Chrome prepends chrome-extension://adcd/
+function stripChromePrefix(str) {
+    return str.replace(CHROME_PREFIX_REGEX, URL_PREFIX);
+}
+
+// remove onmouse* events from insiders
+// prepend URL_PREFIX
+function processInsiders(str) {
+    let removed = str.replace(/ on\w+="[^"]*"/g, '');
+    removed = removed.replace(/insidertrading\.ashx/g, URL_PREFIX + "insidertrading.ashx");
+    return removed;
 }
 
 function isDefined(smth) {
     return typeof smth !== 'undefined';
+}
+
+function arrayBufferToBase64(buffer) {
+    let binary = '';
+    let bytes = new Uint8Array(buffer);
+    let len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
 }
--- content.js
+++ content.js
@@ -37,7 +37,6 @@ chrome.storage.local.get(['show_earnings_surprise', 'ms_style_output', 'limit_nu
 
     // TODO: check if getfromfinviz option is enabled
     chrome.runtime.sendMessage({symbol: 'yes'}, (response) => {
-        console.log("received response", response);
         if (!response.error) {
             fundamentals.push(response); 
             waitForEl("#h_earnings", displayFundamentals, 30); 
@@ -170,7 +169,7 @@ function prepare() {
     }
     .myf {
        float: left;
-       border: 1px solid black;
+       border: 1px solid #3c4144;
        border-collapse: collapse;
        margin: 25px 20px 25px 0px;
        padding-left: 15px;
@@ -179,14 +178,52 @@ function prepare() {
        box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
     }
     .myf tr{
-        border: 1px solid black;
+        border: 1px solid #3c4144;
     }
     .myf td{
-        border: 1px solid black;
+        border: 1px solid #3c4144;
         padding: 10px 5px;
+        width: 12%;
     }
     .fdata {
         text-align: left;
+    }
+    .ftitle {
+        text-align: center;
+        width: 25%;
+    }
+    .fv_chart {
+        width: 900px;
+        height: 340px;
+    }
+    .fv_description {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        max-width: 900px;
+    }
+    .fv_description:hover {
+            white-space: normal;
+        }
+    .body-table-rating-upgrade {
+        color: #04C90A;
+    }
+    .body-table-rating-downgrade {
+        color: #FF0000;
+    }
+    .news-link-container {
+        display: flex;
+    }
+    .news-link-left {
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+    }
+    .news-link-right {
+        padding-left: 4px;
+        display: flex;
+        align-items: flex-end;
+        white-space: nowrap;
     }
     .container {
         overflow: hidden;
@@ -242,30 +279,65 @@ function displayContent() {
 }
 
 function fundamentalsToHtml(data) {
-    const html = `<table class="myf"><tbody>
-        <tr>
-            <td colspan="4">${data.title}</td>
-        </tr>
-        <tr>
-            <td colspan="4">${data.description}</td>
-        </tr>
-        <tr>
-            <td>Mkt Cap</td><td class="fdata">${data.mktcap}</td>
-            <td>ADR%</td><td class="fdata">${data.adr}</td>
-        </tr>
-        <tr>
-            <td>Float</td><td class="fdata">${data.float}</td>
-            <td>Short Float</td><td class="fdata">${data.shorts}</td>
-        </tr>
-        <tr>
-            <td>Inst Own</td><td class="fdata">${data.instown}</td>
-            <td>Earnings</td><td class="fdata">${data.earnings}</td>
-        </tr>
-        <tr>
-            <td>Avg Volume</td><td class="fdata">${data.avgvolume}</td>
-            <td>Rel Volume</td><td class="fdata">${data.relvolume}</td>
-        </tr>
-    </tbody></table>`;
+    const html = `
+        <style>
+            /*! tabbyjs v12.0.3 | (c) 2019 Chris Ferdinandi | MIT License | http://github.com/cferdinandi/tabby */
+            [role=tablist]{border-bottom:1px solid #d3d3d3;list-style:none;margin:0;padding:0}[role=tablist] *{box-sizing:border-box}@media (min-width:30em){[role=tablist] li{display:inline-block}}[role=tab]{border:1px solid transparent;border-top-color:#d3d3d3;display:block;padding:.5em 1em;text-decoration:none}@media (min-width:30em){[role=tab]{border-top-color:transparent;border-top-left-radius:.5em;border-top-right-radius:.5em;display:inline-block;margin-bottom:-1px}}[role=tab][aria-selected=true]{background-color:#d3d3d3}@media (min-width:30em){[role=tab][aria-selected=true]{background-color:transparent;border:1px solid #d3d3d3;border-bottom-color:#fff}}[role=tab]:hover:not([aria-selected=true]){background-color:#f7f7f7}@media (min-width:30em){[role=tab]:hover:not([aria-selected=true]){border:1px solid #d3d3d3}}[hidden]{display:none}
+        </style>
+        <script>
+            /*! tabbyjs v12.0.3 | (c) 2019 Chris Ferdinandi | MIT License | http://github.com/cferdinandi/tabby */
+            Element.prototype.matches||(Element.prototype.matches=Element.prototype.msMatchesSelector||Element.prototype.webkitMatchesSelector),Element.prototype.closest||(Element.prototype.matches||(Element.prototype.matches=Element.prototype.msMatchesSelector||Element.prototype.webkitMatchesSelector),Element.prototype.closest=function(e){var t=this;if(!document.documentElement.contains(this))return null;do{if(t.matches(e))return t;t=t.parentElement}while(null!==t);return null}),(function(e,t){"function"==typeof define&&define.amd?define([],(function(){return t(e)})):"object"==typeof exports?module.exports=t(e):e.Tabby=t(e)})("undefined"!=typeof global?global:"undefined"!=typeof window?window:this,(function(e){"use strict";var t={idPrefix:"tabby-toggle_",default:"[data-tabby-default]"},r=function(t){if(t&&"true"!=t.getAttribute("aria-selected")){var r=document.querySelector(t.hash);if(r){var o=(function(e){var t=e.closest('[role="tablist"]');if(!t)return{};var r=t.querySelector('[role="tab"][aria-selected="true"]');if(!r)return{};var o=document.querySelector(r.hash);return r.setAttribute("aria-selected","false"),r.setAttribute("tabindex","-1"),o?(o.setAttribute("hidden","hidden"),{previousTab:r,previousContent:o}):{previousTab:r}})(t);!(function(e,t){e.setAttribute("aria-selected","true"),e.setAttribute("tabindex","0"),t.removeAttribute("hidden"),e.focus()})(t,r),o.tab=t,o.content=r,(function(t,r){var o;"function"==typeof e.CustomEvent?o=new CustomEvent("tabby",{bubbles:!0,cancelable:!0,detail:r}):(o=document.createEvent("CustomEvent")).initCustomEvent("tabby",!0,!0,r),t.dispatchEvent(o)})(t,o)}}},o=function(e,t){var o=(function(e){var t=e.closest('[role="tablist"]'),r=t?t.querySelectorAll('[role="tab"]'):null;if(r)return{tabs:r,index:Array.prototype.indexOf.call(r,e)}})(e);if(o){var n,i=o.tabs.length-1;["ArrowUp","ArrowLeft","Up","Left"].indexOf(t)>-1?n=o.index<1?i:o.index-1:["ArrowDown","ArrowRight","Down","Right"].indexOf(t)>-1?n=o.index===i?0:o.index+1:"Home"===t?n=0:"End"===t&&(n=i),r(o.tabs[n])}};return function(n,i){var a,l,u={};u.destroy=function(){var e=l.querySelectorAll("a");Array.prototype.forEach.call(e,(function(e){var t=document.querySelector(e.hash);t&&(function(e,t,r){e.id.slice(0,r.idPrefix.length)===r.idPrefix&&(e.id=""),e.removeAttribute("role"),e.removeAttribute("aria-controls"),e.removeAttribute("aria-selected"),e.removeAttribute("tabindex"),e.closest("li").removeAttribute("role"),t.removeAttribute("role"),t.removeAttribute("aria-labelledby"),t.removeAttribute("hidden")})(e,t,a)})),l.removeAttribute("role"),document.documentElement.removeEventListener("click",c,!0),l.removeEventListener("keydown",s,!0),a=null,l=null},u.setup=function(){if(l=document.querySelector(n)){var e=l.querySelectorAll("a");l.setAttribute("role","tablist"),Array.prototype.forEach.call(e,(function(e){var t=document.querySelector(e.hash);t&&(function(e,t,r){e.id||(e.id=r.idPrefix+t.id),e.setAttribute("role","tab"),e.setAttribute("aria-controls",t.id),e.closest("li").setAttribute("role","presentation"),t.setAttribute("role","tabpanel"),t.setAttribute("aria-labelledby",e.id),e.matches(r.default)?e.setAttribute("aria-selected","true"):(e.setAttribute("aria-selected","false"),e.setAttribute("tabindex","-1"),t.setAttribute("hidden","hidden"))})(e,t,a)}))}},u.toggle=function(e){var t=e;"string"==typeof e&&(t=document.querySelector(n+' [role="tab"][href*="'+e+'"]')),r(t)};var c=function(e){var t=e.target.closest(n+' [role="tab"]');t&&(e.preventDefault(),r(t))},s=function(e){var t=document.activeElement;t.matches(n+' [role="tab"]')&&(["ArrowUp","ArrowDown","ArrowLeft","ArrowRight","Up","Down","Left","Right","Home","End"].indexOf(e.key)<0||o(t,e.key))};return a=(function(){var e={};return Array.prototype.forEach.call(arguments,(function(t){for(var r in t){if(!t.hasOwnProperty(r))return;e[r]=t[r]}})),e})(t,i||{}),u.setup(),(function(t){if(!(e.location.hash.length<1)){var o=document.querySelector(t+' [role="tab"][href*="'+e.location.hash+'"]');r(o)}})(n),document.documentElement.addEventListener("click",c,!0),l.addEventListener("keydown",s,!0),u}}));
+            var f_tabs = new Tabby('[data-tabs]');
+        </script>
+        <ul data-tabs>
+            <li><a data-tabby-default href="#f_fundamentals" onmouseover="f_tabs.toggle('#f_fundamentals')">Fundamentals</a></li>
+            <li><a href="#f_chart" onmouseover="f_tabs.toggle('#f_chart')">Chart</a></li>
+            <li><a href="#f_ratings" onmouseover="f_tabs.toggle('#f_ratings')">Ratings</a></li>
+            <li><a href="#f_news" onmouseover="f_tabs.toggle('#f_news')">News</a></li>
+            <li><a href="#f_insiders" onmouseover="f_tabs.toggle('#f_insiders')">Insiders</a></li>
+        </ul>
+
+            <div id="f_fundamentals">  
+                <table class="myf"><tbody>
+                <tr>
+                    <td class="ftitle" colspan="4"><a target="_blank" id="f_ticker" href="${data.tickerHref}">${data.ticker}</a> ${data.exchange}<br/>
+                    ${data.site}<br/>
+                    <a target="_blank" href="${data.sectorHref}">${data.sector}</a> | <a target="_blank" href="${data.industryHref}">${data.industry}</a> | <a target="_blank" href="${data.countryHref}">${data.country}</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="4"><div class="fv_description">${data.description}</div></td>
+                </tr>
+                <tr>
+                    <td>Mkt Cap</td><td class="fdata">${data.mktcap}</td>
+                    <td>ADR</td><td class="fdata">${data.adr}</td>
+                </tr>
+                <tr>
+                    <td>Float</td><td class="fdata">${data.float}</td>
+                    <td>Earnings</td><td class="fdata">${data.earnings}</td>
+                </tr>
+                <tr>
+                    <td>Short Float</td><td class="fdata">${data.shorts}</td>
+                    <td>Inst Own</td><td class="fdata">${data.instown}</td>
+                </tr>
+                <tr>
+                    <td>Avg Volume</td><td class="fdata">${data.avgvolume}</td>
+                    <td>Rel Volume</td><td class="fdata">${data.relvolume}</td>
+                </tr>
+            </tbody></table>
+            </div>
+            <div id="f_chart">
+                <img class="fv_chart" src="data:image/png;base64, ${data.chart}" alt="${data.ticker} chart"/>
+            </div>
+            <div id="f_ratings">
+                ${data.ratings}
+            </div>
+            <div id="f_news">
+                ${data.news}
+            </div>
+            <div id="f_insiders">
+                ${data.insiders}
+            </div>`;
     return html;
 }
 
